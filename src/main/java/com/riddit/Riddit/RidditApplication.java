83cc8d89e7e0e371f2391865cdc678722dfcb122
cassianomaia <cassmala@gmail.com>
2020-03-25 10:10:24 -0300

    Base Project

---
+++ src/main/java/com/riddit/Riddit/RidditApplication.java
@@ -0,0 +1,13 @@
+package com.riddit.Riddit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RidditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RidditApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/riddit/Riddit/RidditApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riddit.Riddit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RidditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
