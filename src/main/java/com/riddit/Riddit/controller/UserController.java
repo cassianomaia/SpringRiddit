3e8df0a74e4cf9422d0036b8a33f865e703c2e69
cassianomaia <cassmala@gmail.com>
2020-03-25 11:38:21 -0300

    User controller and some todo's

--- src/main/java/com/riddit/Riddit/controller/UserController.java
+++ src/main/java/com/riddit/Riddit/controller/UserController.java
@@ -1,13 +1,36 @@
 package com.riddit.Riddit.controller;
 
+import com.riddit.Riddit.model.User;
+import com.riddit.Riddit.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class UserController {
 
-    @RequestMapping({ "/user" })
-    public String firstPage() {
-        return "Hello World";
+    @Autowired
+    private UserService userService;
+
+    // TODO: Remove after user class is completed, this is just to develop purposes
+    @RequestMapping("/user")
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    // TODO: Add password size verification
+    @RequestMapping(method = RequestMethod.POST, value = "/user")
+    public void addUser(@RequestBody User user) {
+        userService.addUser(user);
+    }
+
+    // TODO: Make a way to update the email, as the email is the id
+    @RequestMapping(method = RequestMethod.PUT, value = "/user")
+    public void updateUser(@RequestBody User user) {
+        userService.updateUser(user);
     }
 }
